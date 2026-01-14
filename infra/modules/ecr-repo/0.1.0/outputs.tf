2cf1454b33a358c17d4ae796d3a8affd4f8c9609
paulbradbury <paul@karoosoftware.com>
2026-01-14 09:35:15 +0000

    I had to build the docker config.json outside of the kaniko image as the image does not have apk to install the aws cli

--- infra/modules/ecr-repo/0.1.0/outputs.tf
+++ infra/modules/ecr-repo/0.1.0/outputs.tf
@@ -1,19 +1,11 @@
-data "aws_region" "current" {}
-data "aws_caller_identity" "current" {}
-
-output "repository_name" {
-  value = aws_ecr_repository.this.name
-}
-
-output "repository_arn" {
+output "arn" {
   value = aws_ecr_repository.this.arn
 }
 
-output "repository_url" {
-  value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/${aws_ecr_repository.this.name}"
+output "name" {
+  value = aws_ecr_repository.this.name
 }
 
-output "gitlab_push_role_arn" {
-  value       = var.create_gitlab_push_role ? aws_iam_role.gitlab_push[0].arn : null
-  description = "ARN of the GitLab role allowed to push to this repo"
+output "repository_url" {
+  value = aws_ecr_repository.this.repository_url
 }
